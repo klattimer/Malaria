ebf9b6c3d860b9435753276a9bd7fa0f9d4176a7
Karl Lattimer <karl@qdh.org.uk>
2021-03-01 23:55:57 +0000

    added calculated current speed

--- Malaria/Plugins/network.py
+++ Malaria/Plugins/network.py
@@ -1,17 +1,40 @@
 from Malaria.Plugins import MalariaPlugin
 import psutil
+import time
 import os
 
 
 class Network(MalariaPlugin):
     def __init__(self, malaria, **kwargs):
         super(Network, self).__init__(malaria, **kwargs)
+        self._last_timestamp = None
+        self._last_in = 0
+        self._last_out = 0
 
     def update(self):
         psutil.net_io_counters(pernic=True)
-        self.report_data({
+        data = {
             "addresses": psutil.net_if_addrs(),
             "io": psutil.net_io_counters(pernic=True, nowrap=True),
             "stats": psutil.net_if_stats(),
             "online": True if os.system("ping -c 1 8.8.4.4") is 0 else False
-        })
+        }
+        now = time.time()
+        bytes_in = 0
+        bytes_out = 0
+        for d in data['io'].keys():
+            if d == 'lo': continue
+            bytes_in += data['io'][d]['bytes_recv']
+            bytes_out += data['io'][d]['bytes_sent']
+
+        if self._last_timestamp is not None:
+            duration = now - self._last_timestamp
+            bps_in = (bytes_in - self._last_in) / duration
+            bps_out = (bytes_out - self._last_out) / duration
+            data['io']['download_speed'] = bps_in
+            data['io']['upload_speed'] = bps_out
+
+        self._last_in = bytes_in
+        self._last_out = bytes_out
+        self._last_timestamp = now
+        self.report_data(data)
