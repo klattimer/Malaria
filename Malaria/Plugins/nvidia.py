2d7b1db5e99c62f9864efff441ab43afd8287f8b
Karl Lattimer <karl@qdh.org.uk>
2021-03-29 14:07:25 +0100

    Add HA integration for nvidia

--- Malaria/Plugins/nvidia.py
+++ Malaria/Plugins/nvidia.py
@@ -3,6 +3,14 @@ import subprocess
 import re
 from nesteddictionary import NestedDict
 
+units = {
+    '%': '%',
+    'MiB': 'MiB',
+    'MHz': 'Mhz',
+    ' C': "\u00b0C",
+    'KB/s': 'KB/s'
+}
+
 
 class NVidia(MalariaPlugin):
     def __init__(self, malaria, **kwargs):
@@ -20,20 +28,40 @@ class NVidia(MalariaPlugin):
         kv_parse = re.compile('^(\s+)(.*?)\s\s+:\s+(.*)')
         h_parse = re.compile('^(\s+)(.*)')
         accumulator = {}
+        with_units = {}
         path = ['root']
         # Parse the lines shifting the path as we go
         for line in lines:
             line = '    ' + line
             try:
                 (i_str, key, value) = kv_parse.findall(line)[0]
+                if key == 'Process ID':
+                    raise Exception('Process ID is a header')
                 if (len(i_str) / 4) < len(path):
                     path = path[:-1]
-                accumulator['/'.join(path) + '/' + key] = value
+                p = '/'.join(path) + '/' + key
+
+                # Move units into the key name
+                for unit in units.keys():
+                    if value.endswith(unit):
+                        key += ' (' + units[unit] + ')'
+                        value = value.replace(unit, '').strip()
+                        with_units[key] = {
+                            'path': p,
+                            'unit': units[unit]
+                        }
+                accumulator[p] = value
             except:
                 try:
                     (i_str, header) = h_parse.findall(line)[0]
                     if (len(i_str) / 4) < len(path):
                         path = path[:-1]
+                    if header.startswith('GPU') and path[-1] != 'GPUs':
+                        path.append('GPUs')
+                    if header.startswith('Process ID') and path[-1] != 'Processes':
+                        path.append('Processes')
+                        k, v = header.split(':')
+                        header = v.strip()
                     if len(header.strip()) > 0:
                         path.append(header)
                 except:
@@ -44,4 +72,14 @@ class NVidia(MalariaPlugin):
             if v == 'N/A': continue
             n.insert(k.split('/'), v)
 
-        return n.unnest()['root']
+        data = n.unnest()['root']
+        for k in with_units.keys():
+            self.malaria.register_homeassistant_sensor(
+                with_units[k]['path'],
+                None,
+                k,
+                with_units[k]['unit'],
+                'float',
+                'mdi:gpu'
+            )
+        return data
