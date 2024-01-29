5863b0ba877e07162295258690c18ec50efe81ac
Karl Lattimer <karl@qdh.org.uk>
2024-01-29 18:56:21 +0000

    Improve docker, added sensors

--- Malaria/Plugins/docker.py
+++ Malaria/Plugins/docker.py
@@ -3,13 +3,35 @@ import docker
 
 
 class Docker(MalariaPlugin):
-    __topic__ = "Docker/stats"
-    
+    __topic__ = "Docker"
+
     def __init__(self, malaria, **kwargs):
         super(Docker, self).__init__(malaria, **kwargs)
         self.client = docker.from_env()
+        containers = self.client.containers.list(all=True)
+        for c in containers:
+
+            ha_topic = '/'.join([
+                        self.__topic__,
+                        'status',
+                        c.name
+            ])
+
+            self.malaria.register_homeassistant_sensor(
+                ha_topic,
+                None,
+                f"Container {c.name} status"
+                '',
+                'string'
+            )
 
     def update(self):
-        containers = self.client.containers.list()
-        data = {c.name: c.stats(stream=False) for c in containers}
+        containers = self.client.containers.list(all=True)
+        data = {}
+        data['stats'] = {}
+        data['status'] = {}
+        for c in containers:
+            data['stats'][c.name] = c.stats(stream=False)
+            data['status'][c.name] = c.status
+
         self.report_data(data)
